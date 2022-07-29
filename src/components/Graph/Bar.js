eb5260d92033bd47e3f5a7907c1d9c98d40bcf81
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2022-07-29 14:03:44 +0200

    update bar color with today

--- src/components/Graph/Bar.js
+++ src/components/Graph/Bar.js
@@ -2,6 +2,20 @@ import "./Bar.scss";
 
 const maxBarHeight = 220; // px
 
+function today(day) {
+  const days = {
+    0: "sun",
+    1: "mon",
+    2: "tue",
+    3: "wed",
+    4: "thu",
+    5: "fri",
+    6: "sat"
+  }
+  const dayToday = days[new Date().getDay()];
+  return dayToday === day ? "--today" : "";
+}
+
 function Bar({ result, maxAmount }) {
   return (
     <div className="Bar">
@@ -9,7 +23,7 @@ function Bar({ result, maxAmount }) {
         <p>${result.amount}</p>
       </div>
       <div
-        className="Bar__bar"
+        className={`Bar__bar${today(result.day)}`}
         style={{
           height: `${Math.round((maxBarHeight * result.amount) / maxAmount)}px`,
         }}
